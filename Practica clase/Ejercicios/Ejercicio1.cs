41591a53e299f57fcc9e6e3f350bad847b3ad36f
ValcocerLopez <vmalcocer@uamv.edu.ni>
2025-11-21 01:05:07 -0600

    agregando el ejercicio 1

--- Practica clase/Ejercicios/Ejercicio1.cs
+++ Practica clase/Ejercicios/Ejercicio1.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Practica_clase.Ejercicios
+namespace PracticaClase.Ejercicios
 {
-    internal class Ejercicio_1
+    public static class Ejercicio1
     {
+        public static void Ejecutar()
+        {
+            Random rnd = new Random();
+            int[] arreglo = new int[20];
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = rnd.Next(1, 101);
+                Console.Write(arreglo[i] + " ");
+            }
+
+            Console.WriteLine("\nIngrese un número a buscar:");
+            int numero = int.Parse(Console.ReadLine());
+
+            int posicion = -1;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == numero)
+                {
+                    posicion = i;
+                    break;
+                }
+            }
+
+            if (posicion != -1)
+                Console.WriteLine($"Número encontrado en la posición {posicion}");
+            else
+                Console.WriteLine("Número no encontrado");
+        }
     }
 }
